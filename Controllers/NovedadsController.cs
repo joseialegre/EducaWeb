a85958ffe0c209e9711bc723147b43c216914603
José Ignacio Alegre <joseignacioalegre4@gmail.com>
2023-08-20 23:38:32 -0300

    fix-create-novedades

--- Controllers/NovedadsController.cs
+++ Controllers/NovedadsController.cs
@@ -58,7 +58,7 @@ namespace WebApplication1.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Description")] Novedad novedad)
+        public async Task<IActionResult> Create([Bind("Id,Description,Publisher,Date")] Novedad novedad)
         {
             if (ModelState.IsValid)
             {
